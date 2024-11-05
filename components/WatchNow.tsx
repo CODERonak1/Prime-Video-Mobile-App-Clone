ac1efc52fb3032b55bfbac6ca80dcd27de09347f
r9988.2588 <r9988.2588@gmail.com>
2024-11-05 02:55:59 +0000

    added 6 images and watch now component

---
+++ components/WatchNow.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const WatchNow = () => {
+  return (
+    <View>
+      <Text>WatchNow</Text>
+    </View>
+  )
+}
+
+export default WatchNow;
+
+const styles = StyleSheet.create({})
